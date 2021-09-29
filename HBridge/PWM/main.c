7ff6c406f29e51e2c392eef0b37b2f38f89933f2
Tomasz <swiechowicz.tomasz@gmail.com>
2021-09-29 11:05:47 +0200

    fixed project

--- HBridge/PWM/main.c
+++ HBridge/PWM/main.c
@@ -1,3 +1,10 @@
+/*
+ * Bridge.c
+ *
+ 
+ * Created: 26/09/2019 16:12:28
+ * Author : FRA
+ */ 
 #define F_CPU 10000000
 
 #include <avr/io.h>                        /* Defines pins, ports, etc */
@@ -9,14 +16,11 @@
 
 void CLK_ISO_init(void)
 {
-	TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm; //Enable Split mode
+	TCA0.SPLIT.CTRLD = TCA_SPLIT_SPLITM_bm;
 	TCA0.SPLIT.CTRLB = TCA_SPLIT_HCMP0EN_bm;
-	TCA0.SPLIT.HPER = 99;
-	TCA0.SPLIT.HCMP0 = 0x0; 
-	TCA0.SPLIT.CTRLA = TCA_SPLIT_CLKSEL_DIV1_gc | TCA_SPLIT_ENABLE_bm; //Clock Select and Enable the peripheral
-	GPIO_DIR(0 + 3, 1); //port a 3, output   
-	// w-r  (    , 0)   0=read  1=writ
-	// WO[n+3] Out
+	TCA0.SPLIT.HPER = 99;	TCA0.SPLIT.HCMP0 = 0x0;	TCA0.SPLIT.CTRLA = TCA_SPLIT_CLKSEL_DIV1_gc | TCA_SPLIT_ENABLE_bm;
+	GPIO_DIR(0 + 3, 1);
+	
 	_delay_ms(1);
 	
 	while(TCA0.SPLIT.HCMP0 < (TCA0.SPLIT.HPER+1) >> 1)
@@ -34,25 +38,25 @@ int main(void)
 	CPU_CCP = 0xD8;
 	CLKCTRL.MCLKCTRLB = 1;
 	/*END setting clock*/
-	
+		
 	CLK_ISO_init();
-	
+		
 	Comm_init();
 
 	Temp_meas_init();
 
 	sei();
 
-	while (1)
-	{
+    while (1)
+    {
 		Temp_meas();
 		cli();
 		memcpy(valid_data_tx, Temps, 8);
-	    sei();
-		/*if (TCB0.CNT > 7500)
+		sei();
+		if (TCB0.CNT > 7500)
 		{
 			*(uint8_t *)&Outputs = 0;
 			IO_update();
-		}*/
-	}
+		}
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-AttinyPhD
+HBridge
