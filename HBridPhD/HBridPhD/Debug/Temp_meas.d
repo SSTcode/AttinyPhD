63ddbfd3490088a82f96e6effb9ae42e88ae16c2
FLRA <felipe.ruizal@sansano.usm.cl>
2021-09-29 10:09:16 -0300

    First 2

---
+++ HBridPhD/HBridPhD/Debug/Temp_meas.d
@@ -0,0 +1,48 @@
+Temp_meas.d Temp_meas.o: .././Temp_meas.c .././Temp_meas.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h \
+ C:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/Atmel/ATtiny_DFP/1.9.337/xc8/avr/include/avr/iotn402.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\xmega.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdlib.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h
+
+.././Temp_meas.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdint.h:
+
+C:/Program\ Files\ (x86)/Atmel/Studio/7.0/Packs/Atmel/ATtiny_DFP/1.9.337/xc8/avr/include/avr/iotn402.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\version.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\stdlib.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\microchip\xc8\v2.31\avr\avr\include\math.h:
